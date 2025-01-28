4611305bdad58a568afc69d87b4ec2bf039e71a6
Sijan Pradhan <106319644+CodingWithSijan@users.noreply.github.com>
2025-01-28 17:06:57 +1100

    Update emailController.js
    email service changed to hotmail

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -5,7 +5,7 @@ const sendEmail = async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      service: "hotmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
