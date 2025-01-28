af2797d89a1e0b405acf324283ee80ab483716a2
Sijan Pradhan <106319644+CodingWithSijan@users.noreply.github.com>
2025-01-28 19:18:58 +1100

    Update emailController.js
    mail transport update

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -5,7 +5,9 @@ const sendEmail = async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "hotmail",
+      host: 'smtp-mail.outlook.com',
+      port: 587,
+      secure: false, // Use true for port 465
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
